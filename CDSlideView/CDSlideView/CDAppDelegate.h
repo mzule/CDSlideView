adb039ed44807025fe368157a5c4fbb2d36d730c
Jason Cao <mzule4j@gmail.com>
2014-03-10 17:23:36 +0800

    first commit

---
+++ CDSlideView/CDSlideView/CDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CDAppDelegate.h
+//  CDSlideView
+//
+//  Created by mzule on 3/10/14.
+//  Copyright (c) 2014 mzule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CDSlideView/CDSlideView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CDSlideView
+//
+//  Created by mzule on 3/10/14.
+//  Copyright (c) 2014 mzule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CDAppDelegate class]));
+    }
+}
